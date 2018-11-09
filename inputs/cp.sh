b8f4769a2002e9105cf25169aed57e7aa0c21602
Benjamin <benjaminkreis@gmail.com>
2018-11-09 14:04:57 -0600

    real systematics on transfer factors -- but not sure about inputs

--- inputs/cp.sh
+++ inputs/cp.sh
@@ -1,6 +1,7 @@
-cp ~/workspace/LLDJ/analyzer/oct10/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/PhoPT170_k1p26/EleMuOSOF/GH/EleMuOSOF_nSelectedAODCaloJetTag_GH.root .
-cp ~/workspace/LLDJ/analyzer/oct10/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/PhoPT170_k1p26/OnePho/GH/OnePho_nSelectedAODCaloJetTag_GH.root .
-cp ~/workspace/LLDJ/analyzer/oct10/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/PhoPT170_k1p26/TwoMuZH/GH/TwoMuZH_nSelectedAODCaloJetTag_GH.root .
-cp ~/workspace/LLDJ/analyzer/oct10/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/PhoPT170_k1p26/TwoEleZH/GH/TwoEleZH_nSelectedAODCaloJetTag_GH.root .
-cp ~/workspace/LLDJ/analyzer/oct10/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/PhoPT170_k1p26/TwoMuDY/GH/TwoMuDY_nSelectedAODCaloJetTag_GH.root .
-cp ~/workspace/LLDJ/analyzer/oct10/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/PhoPT170_k1p26/TwoEleDY/GH/TwoEleDY_nSelectedAODCaloJetTag_GH.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/EleMuOSOF/GH/EleMuOSOF_nSelectedAODCaloJetTag_GH_*.root .
+#cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/OnePho/GH/OnePho_nSelectedAODCaloJetTag_GH_*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/TwoMuZH/GH/TwoMuZH_nSelectedAODCaloJetTag_GH_*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/TwoEleZH/GH/TwoEleZH_nSelectedAODCaloJetTag_GH_*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/TwoMuDY/GH/TwoMuDY_nSelectedAODCaloJetTag_GH_*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/TwoEleDY/GH/TwoEleDY_nSelectedAODCaloJetTag_GH_*.root .
+
