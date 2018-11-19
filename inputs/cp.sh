581020ff59075df6ab61f2f7cec9f8d7f7a07b70
Benjamin <benjaminkreis@gmail.com>
2018-11-19 11:07:34 -0600

    update to copy systs

--- inputs/cp.sh
+++ inputs/cp.sh
@@ -1,7 +1,7 @@
-cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/EleMuOSOF/GH/EleMuOSOF_nSelectedAODCaloJetTag_GH*.root .
-cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/OnePho/GH/OnePho_nSelectedAODCaloJetTag_GH*.root .
-cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/TwoMuZH/GH/TwoMuZH_nSelectedAODCaloJetTag_GH*.root .
-cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/TwoEleZH/GH/TwoEleZH_nSelectedAODCaloJetTag_GH*.root .
-cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/TwoMuDY/GH/TwoMuDY_nSelectedAODCaloJetTag_GH*.root .
-cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusEMsh/TwoEleDY/GH/TwoEleDY_nSelectedAODCaloJetTag_GH*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusDYsh/EleMuOSOF/GH/EleMuOSOF_nSelectedAODCaloJetTag_GH*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusDYsh/OnePho/GH/OnePho_nSelectedAODCaloJetTag_GH*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusDYsh/TwoMuZH/GH/TwoMuZH_nSelectedAODCaloJetTag_GH*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusDYsh/TwoEleZH/GH/TwoEleZH_nSelectedAODCaloJetTag_GH*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusDYsh/TwoMuDY/GH/TwoMuDY_nSelectedAODCaloJetTag_GH*.root .
+cp ~/workspace/LLDJ/analyzer/nov7/LLDJ_slc6_530_CMSSW_8_0_26_patch1/src/LLDJstandalones/plots/mrtusDYsh/TwoEleDY/GH/TwoEleDY_nSelectedAODCaloJetTag_GH*.root .
 
