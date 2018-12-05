63002f1d9f4570dd6c12b977110816507d9aa75d
Benjamin <benjaminkreis@gmail.com>
2018-12-04 21:22:12 -0600

    For limit plots

--- model/run.sh
+++ model/run.sh
@@ -6,14 +6,20 @@ do
     echo -e "\n Starting $i"
 
     #Prepare output area
-    rm -rf "model_$i"
-    mkdir "model_$i"
+    mydir="model_$i"
+    rm -rf $mydir
+    mkdir $mydir
     
     rm -rf param_ws.root
     root -l -b -q "build_ws.C+(\"$i\")"
     
-    cp param_ws.root model_$i/.
-    cp card_*.txt model_$i/.
+    cp param_ws.root $mydir/.
+    cp card_*.txt $mydir/.
     
+    combineCards.py EleMu=$mydir/card_elemu.txt EleMuL=$mydir/card_elemul.txt TwoMuDY=$mydir/card_twomudy.txt TwoEleDY=$mydir/card_twoeledy.txt TwoMuZH=$mydir/card_twomuzh.txt TwoEleZH=$mydir/card_twoelezh.txt > $mydir/card.txt
+
+    text2workspace.py $mydir/card.txt -o $mydir/card.root
+
+    combine -M AsymptoticLimits $mydir/card.root -n ".$i" --run blind
 
 done
