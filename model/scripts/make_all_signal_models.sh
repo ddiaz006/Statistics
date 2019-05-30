c9fb85e439e6c77748b8c994b364ad4b20acdebc
cmorgoth <cmorgoth@fnal.gov>
2019-05-30 10:54:15 -0500

    add additional scripts

---
+++ model/scripts/make_all_signal_models.sh
@@ -0,0 +1,73 @@
+#! /bin/bash
+
+while [[ $# -gt 1 ]]
+do
+    #echo blash $#
+    key="$1"
+    #echo $key " " $2
+    
+    case $key in
+	-il|--input_list)
+	    input_list="$2"
+	        shift #past argument
+		    ;;
+	-od|--output_dir)
+	    output_dir="$2"
+	        shift # past argument
+		    ;;
+	-i2|--inputDir2)
+	    indir2="$2"
+	        shift # past argument
+		    ;;
+	-o|--outputFile)
+	    outfile="$2"
+	        shift # past argument
+		    ;;
+	-d|--dataFile)
+	    datafile="$2"
+	        shift # past argument
+		    ;;
+	-smh|--smhFile)
+	    smhfile="$2"
+	        shift # past argument
+		    ;;
+	-s|--signalFile)
+	    sfile="$2"
+	        shift # past argument
+		    ;;
+	-b|--binningFile)
+	    bfile="$2"
+	        shift # past argument
+		    ;;
+	--default)
+	    DEFAULT=YES
+	        ;;
+	*)
+            # unknown option
+	    ;;
+    esac
+    shift # past argument or value
+done
+
+
+echo INPUT LIST  = $input_list
+echo OUTFILE DIR = $output_dir
+
+mkdir -p $output_dir
+while read model; do
+  echo $model
+  mkdir -p $output_dir/$model
+  cp card_*.txt $output_dir/$model
+  ./MakeWorkspace --signal_model=$model
+  mv param_ws.root $output_dir/$model
+  cd $output_dir/$model
+  combineCards.py EleMu=card_elemu.txt EleMuL=card_elemul.txt TwoMuDY=card_twomudy.txt TwoEleDY=card_twoeledy.txt TwoMuZH=card_twomuzh.txt TwoEleZH=card_twoelezh.txt > card.txt
+  text2workspace.py card.txt
+  combine -M AsymptoticLimits card.root --run blind
+  cd -
+done <$input_list
+
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 $1
+fi
